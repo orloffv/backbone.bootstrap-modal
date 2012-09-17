ec28827aba2418b802e0ff1adc591db4c39dd8a9
Zouyingjun <zou5945@gmail.com>
2012-09-17 10:29:31 +0800

    Update src/backbone.bootstrap-modal.js
    fix do not trigger "hidden" event when close modal on chrome.

--- src/backbone.bootstrap-modal.js
+++ src/backbone.bootstrap-modal.js
@@ -194,13 +194,13 @@
         return;
       }
 
-      $el.modal('hide');
-
       $el.one('hidden', function() {
         self.remove();
 
         self.trigger('hidden');
       });
+      
+      $el.modal('hide');
 
       Modal.count--;
     },
